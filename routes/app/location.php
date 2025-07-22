530975770199be86c7fb780cf1e03edb8aadddfe
taranjitsahota <taranjit.sahota@gmail.com>
2025-07-22 22:12:09 +0530

    district api fixed update

--- routes/app/location.php
+++ routes/app/location.php
@@ -6,12 +6,12 @@ use App\Http\Controllers\LocationController;
 // add this in middleware
 
 Route::middleware('auth:sanctum')->group(function () {
-    // Route::middleware('role:superadmin')->group(function () {
+    Route::middleware('role:user')->group(function () {
         Route::prefix('location')->controller(LocationController::class)->group(function () {
             Route::get('/states', 'getStates');
             Route::get('/districts/{state_id}', 'getDistricts');
             Route::get('/tehsils/{district_id}', 'getTehsils');
             Route::get('/villages/{tehsil_id}', 'getServicableVillages');
         });
-    // });
+    });
 });
